8cf86d386b8268fa5c58dbd51bec7da4cac2923b
Ar0gs <102906590+Ar0gs@users.noreply.github.com>
2026-05-23 18:24:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,6 +10,8 @@
 
 const SUPABASE_URL = 'https://ltwmlgmipvjfboagwyct.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0d21sZ21pcHZqZmJvYWd3eWN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzODE1NzgsImV4cCI6MjA5NDk1NzU3OH0.0kcj6mGPoweVblmjYxPUs_KKRZUvn_cdyO9qjgF2J4o';
+const VAPID_PUBLIC_KEY = 'BOsAWqVtL3Pfb0WDOxrF2Ex8NnF59MfP_S4ASwgz-EKaGB1mzmK4MNDUlT17aoBF8rJXK_G7bAWhgbmaOksHgMY';
+window.VAPID_PUBLIC_KEY = VAPID_PUBLIC_KEY;
 
 // Admin email — this user gets counsellor/admin privileges
 // After signing up with this email, they see the admin dashboard
